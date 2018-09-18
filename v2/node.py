e07ac5f45dd5b719e2184572d0a221e75d7d8761
Luis Porras <luis.porrasledezma@ucr.ac.cr>
2018-09-17 23:52:55 -0600

    Add a new v2 and the utilities.py file

--- v2/node.py
+++ v2/node.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 from threading import Thread
 from socket import *
+from utilities import *
 
 # Clases
 class ClientNode():
@@ -15,16 +16,16 @@ class ClientNode():
     # Then we ask for the number of lines of the message(n)
     # Then we ask for each line of the message
     # Finally this method returns a str with the format desided((n)\n(<ip>/<mask>/<cost>)\n...)
-    def askUserMessage():
+    def askUserMessage(self):
         print("ClientNode : Give it to me!")
     
     # Pack the message given by the user in the requested format: n (2 bytes), ip (4 bytes), mask(1 byte), cost (3 bytes)
     # Returns the packed message
-    def packMessage():
+    def packMessage(self):
         print("ClientNode : Packing the message ...")
 
     # Send the packed message past by the user and send it to the destination also past by the user.	
-    def sendMessage():
+    def sendMessage(self):
         print("ClientNode : Sending message")
 
 class ClientNodeUDP():
@@ -54,10 +55,10 @@ class ServerNode(Thread):
         # Missing alcanzability table field
         print("ServerNode : Constructor :(")
     
-    def receiveMessage():
+    def receiveMessage(self):
         print("ServerNode : Receiving shit and stuff!")
 
-    def unpackMessage():
+    def unpackMessage(self):
         print("ServerNode : Unpacking the message ...")
 
     # Update the alcanzavility table structure with the data of the recieved message
@@ -81,35 +82,68 @@ class ServerNodeTCP(ServerNode):
     def __init__(self, port):
         ServerNode.__init__(self, port)
         # Missing alcanzability table field
-    print("ServerNodeTCP : Constructor :(")
+        print("ServerNodeTCP : Constructor :(")
     
     # Overwite the father class relevant methods!
 
 class Node():
     
     # Constructor
-    def __init__(self, nodeType, ip, mask, port):
-        self.nodeType = nodeType
+    def __init__(self, isPseudoBGP, ip, mask, port):
+        self.isPseudoBGP = isPseudoBGP
         self.ip = ip
         self.mask = mask
         self.port = port
         #self.alcanzabilityTable = decided structure
         print("Node (The real mvp!) : Constructor ")
     
-    def printAlcanzabilityTable():
+    def printAlcanzabilityTable(self):
         print("Node : Table :v")    
 
-
     # This is the method that execute everything the program need to work
-    def run():
+    def run(self):
         print("Node : I basically do everything")
 
 # Program main funtion
 if __name__ == '__main__':
-    c0 = ClientNodeUDP()
-    c1 = ClientNodeTCP()
-    s0 = ServerNodeUDP(12000)
-    s1 = ServerNodeTCP(5000)
-    n0 = Node("PseudoBGP", "125.0.0.1", 30, 12000)
+    # We need to parse the arguments pass by the user
+    parser = argparse.ArgumentParser(description='choose the type of node you want to use')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-tcp", "--pseudoBGP", action="store_true")
+    group.add_argument("-udp", "--intAS", action="store_true")
+    parser.add_argument("ip", help="recive the node ip address")
+    parser.add_argument("mask", help="recive the subnet mask, it must be a integer between 8 and 30", type=int)
+    parser.add_argument("port", help="recive the server port number", type=int)
+    args = parser.parse_args()
+    print ("ip address: " + args.ip + "\nsubnet mask: " + str(args.mask) + "\nport number: " + str(args.port))
+
+    # We need to make sure the arg pass by the user are valid
+
+    # We need to check if the user select a node type
+    if(not args.pseudoBGP and not args.intAS):
+        # If the user did not select a node type, the node type will be pseudoBGP by default
+        args.pseudoBGP = True
+
+    # We need to check if the subnet mask pass by the user is valid, ie is in the range [8, 30]
+    if(args.mask < 8 or args.mask > 30):
+        print_error_invalid_mask()
+        sys.exit(-1)
+    print ("The provided subnet mask is valid! Hooray!")
+
+    # We need to check if the port pass by the user is valid, ie is in the range [1, 65535]
+    if(args.port < 0 or args.port > 65535):
+        print_error_invalid_port()
+        sys.exit(-1)
+    print ("The provided port is valid! Hooray!")
+
+    # We need to check if the ip address pass by the user is a valid ip address
+    if(not validate_ip_address(args.ip)):
+        print_error_invalid_ip()
+        sys.exit(-1)
+    print ("The provided ip address is valid! Hooray!")
+
+    # If all the arguments pass by the user are valid, we continue creating the node and executing it
+    node = Node(args.pseudoBGP, args.ip, args.mask, args.port)
+    node.run()
 
     print('Main Terminating...')	
---
+++ v2/utilities.py
@@ -0,0 +1,25 @@
+def print_error_invalid_ip():
+    print("Error: invalid ip address")
+
+def print_error_invalid_mask():
+    print("Error: invalid subnet mask")
+
+def print_error_invalid_port():
+    print("Error: invalid port")
+
+def validate_ip_address(ip):
+    # Fisrt we split the string with the ip address, using the dot as separator, to obtain the decimal parts of the ip address
+    ipTokens = ip.split(".")
+    # We need to make sure the number of decimal parts is exacly four
+    #print(ipTokens)
+    if(not(len(ipTokens) == 4)):
+        return False
+    # We need to check the ip decimal parts are valid, ie are in the range [0, 255]
+    for i in range(0,4):
+        try:
+            decimalPart = int(ipTokens[i])
+        except ValueError:
+            return False
+        if(decimalPart < 0 or decimalPart > 255):
+            return False
+    return True
