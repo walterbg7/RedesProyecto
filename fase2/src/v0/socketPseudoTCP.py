a5ae9a46e7dc3f592c2aaae0c240a1bc9b6d7f27
LuisPorrasL <luis.porrasledezma@ucr.ac.cr>
2018-10-05 11:13:17 -0600

    Add socketPseudoTCP.py file

---
+++ fase2/src/v0/socketPseudoTCP.py
@@ -0,0 +1,59 @@
+from socket import *
+
+class SocketPseudoTCP:
+    
+    # Constructor
+    def __init__(self):
+        # Fields
+        # selfAddr, self ip addr and port
+        # connectioAddr, connection ip addr and port
+        # messageQueue, Queue to store the recived message to this sockect
+        # connectionSockets, dictionary with key: a connectionAddr, and with value: instance of this class
+        self.connectionSockets = {}   
+        print("SocketPseudoTCP : Constructor :)")
+    
+    # Methods
+
+    # "Client" side
+    # connect, starts and finnishes the 3-way handshake with the server socket
+    def connect(self, serverAddr):
+        print("SocketPseudoTCP : Connecting!")
+        pass
+
+    # send, sends a byte array or encoded message.
+    def send(self, message):
+        print("SocketPseudoTCP : Sending!")
+        pass
+
+    # recv, recive the number of bytes passed as arg. Returns a the received message as encoded message o a byte array
+    def recv(self, numberOfBytes):
+        print("SocketPseudoTCP : Receiving!")
+        pass
+
+    # close, closes the connection, ie delete the connection sockect from the connectionSockets dictionary.
+    def close(self):
+        print("SocketPseudoTCP : Closing!")
+        pass
+
+    # "Server" side
+    # bind, calls the bind method of the UDP port and starts the despacher
+    def bind(self, selfaddr):
+        print("SocketPseudoTCP : Binding!")
+        pass
+
+    # listen, thread!, search the server socket messageQueue for a SYN message and proccess it.
+    def listen(self, serverQueueSize):
+        print("SocketPseudoTCP : Listening!")
+        pass
+
+    # accept, returns a instance of this class as the connection with the client socket that initiated the communication. Add the new connectionSocket (instance) to the connectionSockets dictionary
+    def accept(self):
+        print("SocketPseudoTCP : Accepting!")
+        pass 
+
+    # Private methods
+    # despatch, thread!, demultiplex all the messages send to the serverPort. 
+    def despatch(self):
+        print("SocketPseudoTCP : Despatching!")
+        pass
+
